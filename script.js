0813762823a4d3383e2d19081c80c77c16e9fdda
Xayot420 <asatullayevasamandar@gmail.com>
2024-12-19 13:10:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const num1 = ["Doniyor" , "Ismoil" , "Said" , "Mahmud" , "Boisxon"]
+const num = prompt("Ism qoshing");
+const anwer = prompt("Iltimos tanlang: \n 1) push \n 2) pop \n 3) shift \n 4) unshift")
+
+if(anwer == 1){
+    alert(Javob: ${num1.push(num)})
+    console.log(num1);
+    
+}else if(anwer == 2){
+    alert(Javob: ${num1.pop()})
+    console.log(num1);
+    
+}else if(anwer == 3){
+    alert(Javob: ${num1.shift(num)})
+    console.log(num1);
+    
+}else if(anwer == 4){
+    alert(Javob: ${num1.unshift()})
+    console.log(num1);
+    
+}else if(num1 == 0){
+    alert("Iltimos kiriting")
+}else if(num1 == 0){
+    alert("Iltimos kiriting")
+}
+else{
+    alert("Qiymat kiriting")
+}
